be8e2a26ce36b72a248cbfe2cd3b343f601bc67f
Andrii Serputko <aserputko@gmail.com>
2026-01-22 18:09:00 +0200

    feat: enhance frontend with Tailwind CSS, Shadcn UI components

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,20 +1,10 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <h1>Casino Jackpot</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-        </div>
-      </div>
-    </>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Casino Jackpot</h1>
+    </div>
   )
 }
 
